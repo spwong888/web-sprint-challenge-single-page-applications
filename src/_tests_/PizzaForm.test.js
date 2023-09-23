3127a1509e932ff86301672081deabca7342e1e6
Spencer Wong <sawong887@gmail.com>
2023-09-22 18:06:55 -0700

    version 5

--- src/_tests_/PizzaForm.test.js
+++ src/_tests_/PizzaForm.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import PizzaForm from './PizzaForm';
 
-test('Form has validation for #name-input with error message "name must be at least 2 characters"', () => {
+test('Form has validation for #name-input with error message "name must be at least 2 characters"', async () => {
   const { getByLabelText, getByText } = render(<PizzaForm />);
 
   const nameInput = getByLabelText('Name:');
@@ -12,4 +12,9 @@ test('Form has validation for #name-input with error message "name must be at le
   const errorMessage = getByText(/name must be at least 2 characters/i);
 
   expect(errorMessage).toBeInTheDocument();
+
+  await waitFor(() => {
+    const updatedErrorMessage = getByText('name must be at least 2 characters');
+    expect(updatedErrorMessage).toBeInTheDocument();
+  });
 });
